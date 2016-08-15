d089957f88903b1666e84a81409ab064ae81a06c
gc <gc>
2016-08-15 10:53:19 +0200

    spring project init

---
+++ src/main/java/zzpj/CinemaParserApplication.java
@@ -0,0 +1,12 @@
+package zzpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaParserApplication.class, args);
+	}
+}
