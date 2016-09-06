2903eae70d30298a7fbdf63c533b15f47f2ab29e
GC08 <gniewekciolek@gmail.com>
2016-09-06 05:55:46 +0200

    Dodano jsoup

--- src/main/java/zzpj/CinemaParserApplication.java
+++ src/main/java/zzpj/CinemaParserApplication.java
@@ -1,12 +1,22 @@
 package zzpj;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CinemaParserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CinemaParserApplication.class, args);
-	}
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(CinemaParserApplication.class, args);
+
+        Document htmlDoc = Jsoup.connect("http://www.filmweb.pl/film/%C5%BBo%C5%82nierze+kosmosu+II-2004-109485").get();
+        Elements elements = htmlDoc.select("body");
+        Element element = elements.select("filmPlot text").first();
+        System.out.println(element.toString());
+    }
 }
