2408bef516d6326b424ab4702300ac07dd9c5a70
GC08 <gniewekciolek@gmail.com>
2016-09-07 23:07:28 +0200

    Merge origin/master
    Conflicts:
    	src/main/java/zzpj/entities/FilmEntity.java

--- src/main/java/zzpj/entities/FilmEntity.java
+++ src/main/java/zzpj/entities/FilmEntity.java
@@ -3,6 +3,8 @@ package zzpj.entities;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FilmEntity extends Entity {
 
