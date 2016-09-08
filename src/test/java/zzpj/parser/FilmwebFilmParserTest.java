8670ccdabaddbfe3b4571c0aaa4dcea8bef83f7e
GC08 <gniewekciolek@gmail.com>
2016-09-08 09:30:12 +0200

    poprawa testów

--- src/test/java/zzpj/parser/FilmwebFilmParserTest.java
+++ src/test/java/zzpj/parser/FilmwebFilmParserTest.java
@@ -13,54 +13,55 @@ import zzpj.entities.FilmEntity;
 
 public class FilmwebFilmParserTest {
 
-	@Test
-	public void test1() {
-		
-		FilmEntity filmEntity = new FilmEntity();
-		FilmEntity spy = spy(filmEntity);
-		
-		String title = "http://www.filmweb.pl/film/%C5%BBo%C5%82nierze+kosmosu-1997-207#";
-		
-		FilmwebFilmParser filmwebFilmParser=Mockito.mock(FilmwebFilmParser.class);
-		
+    @Test
+    public void test1() {
+
+        FilmEntity filmEntity = new FilmEntity();
+        FilmEntity spy = spy(filmEntity);
+
+        String title = "http://www.filmweb.pl/film/%C5%BBo%C5%82nierze+kosmosu-1997-207#";
+
+        FilmwebFilmParser filmwebFilmParser = Mockito.mock(FilmwebFilmParser.class);
+
         when(filmwebFilmParser.getFilmByTitleIfExists(title)).thenReturn(spy);
 
         assertEquals(filmwebFilmParser.getFilmByTitleIfExists(title), spy);
-		
-	}
-	
-	@Test
-	public void test2() {
-		
-		FilmEntity filmEntity = new FilmEntity();
-		FilmEntity spy = spy(filmEntity);
-		
-		FilmwebFilmParser filmwebFilmParser=Mockito.mock(FilmwebFilmParser.class);
-		
+
+    }
+
+    @Test
+    public void test2() {
+
+        FilmEntity filmEntity = new FilmEntity();
+        FilmEntity spy = spy(filmEntity);
+
+        FilmwebFilmParser filmwebFilmParser = Mockito.mock(FilmwebFilmParser.class);
+
         when(filmwebFilmParser.getFilm()).thenReturn(spy);
 
         assertEquals(filmwebFilmParser.getFilm(), spy);
-		
-	}
-	
-	@Test
-	public void test3() throws IOException {
-		
+
+    }
+
+    @Test
+    public void test3() throws IOException {
+
         FilmwebFilmParser filmParser = new FilmwebFilmParser("http://www.filmweb.pl/film/%C5%BBo%C5%82nierze+kosmosu-1997-207");
         FilmEntity film = filmParser.getFilm();
-        when(film.getTitle()).thenReturn("Żołnierze kosmosu");
-        assertEquals(film.getTitle(), "Żołnierze kosmosu");
-		
-	}
-	
-	@Test
-	public void test4() throws IOException {
-		
+        String title = film.getTitle();
+        //when(title).thenReturn("Żołnierze kosmosu");
+        assertEquals(title, "Żołnierze kosmosu");
+
+    }
+
+    @Test
+    public void test4() throws IOException {
+
         FilmwebFilmParser filmParser = new FilmwebFilmParser("http://www.filmweb.pl/film/%C5%BBo%C5%82nierze+kosmosu-1997-207");
         FilmEntity film = filmParser.getFilm();
-        when(film.getDescription()).thenReturn("Grupa szkolnych przyjaciół wstępuje do armii, co gwarantuje im obywatelstwo i różne przywileje. Wkrótce stawiają czoła najeźdźcom z kosmosu, olbrzymim pajęczakom. ");
+//        when(film.getDescription()).thenReturn("Grupa szkolnych przyjaciół wstępuje do armii, co gwarantuje im obywatelstwo i różne przywileje. Wkrótce stawiają czoła najeźdźcom z kosmosu, olbrzymim pajęczakom. ");
         assertEquals(film.getDescription(), "Grupa szkolnych przyjaciół wstępuje do armii, co gwarantuje im obywatelstwo i różne przywileje. Wkrótce stawiają czoła najeźdźcom z kosmosu, olbrzymim pajęczakom. ");
-		
-	}
+
+    }
 
 }
