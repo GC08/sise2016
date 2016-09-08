5356c763c12e5f2206a5caf593aaf1ac5c6f26ed
Zuzanna Urban <zuzanna.s.urban@gmail.com>
2016-09-08 09:14:20 +0200

    add title and description test to Filmweb

--- src/test/java/zzpj/parser/FilmwebFilmParserTest.java
+++ src/test/java/zzpj/parser/FilmwebFilmParserTest.java
@@ -1,6 +1,9 @@
 package zzpj.parser;
 
 import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -16,7 +19,7 @@ public class FilmwebFilmParserTest {
 		FilmEntity filmEntity = new FilmEntity();
 		FilmEntity spy = spy(filmEntity);
 		
-		String title = "Inside Out";
+		String title = "http://www.filmweb.pl/film/%C5%BBo%C5%82nierze+kosmosu-1997-207#";
 		
 		FilmwebFilmParser filmwebFilmParser=Mockito.mock(FilmwebFilmParser.class);
 		
@@ -37,6 +40,26 @@ public class FilmwebFilmParserTest {
         when(filmwebFilmParser.getFilm()).thenReturn(spy);
 
         assertEquals(filmwebFilmParser.getFilm(), spy);
+		
+	}
+	
+	@Test
+	public void test3() throws IOException {
+		
+        FilmwebFilmParser filmParser = new FilmwebFilmParser("http://www.filmweb.pl/film/%C5%BBo%C5%82nierze+kosmosu-1997-207");
+        FilmEntity film = filmParser.getFilm();
+        when(film.getTitle()).thenReturn("Żołnierze kosmosu");
+        assertEquals(film.getTitle(), "Żołnierze kosmosu");
+		
+	}
+	
+	@Test
+	public void test4() throws IOException {
+		
+        FilmwebFilmParser filmParser = new FilmwebFilmParser("http://www.filmweb.pl/film/%C5%BBo%C5%82nierze+kosmosu-1997-207");
+        FilmEntity film = filmParser.getFilm();
+        when(film.getDescription()).thenReturn("Grupa szkolnych przyjaciół wstępuje do armii, co gwarantuje im obywatelstwo i różne przywileje. Wkrótce stawiają czoła najeźdźcom z kosmosu, olbrzymim pajęczakom. ");
+        assertEquals(film.getDescription(), "Grupa szkolnych przyjaciół wstępuje do armii, co gwarantuje im obywatelstwo i różne przywileje. Wkrótce stawiają czoła najeźdźcom z kosmosu, olbrzymim pajęczakom. ");
 		
 	}
 
