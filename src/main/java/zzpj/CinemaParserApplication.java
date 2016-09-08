5cfeed08f5573a0b3217f06093bc7a8e7e292b44
GC08 <gniewekciolek@gmail.com>
2016-09-08 09:22:29 +0200

    load data from base to entity fix#2

--- src/main/java/zzpj/CinemaParserApplication.java
+++ src/main/java/zzpj/CinemaParserApplication.java
@@ -14,17 +14,16 @@ public class CinemaParserApplication {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(CinemaParserApplication.class, args);
-        CinemaCityRepertoireParser cinema = new CinemaCityRepertoireParser();
-
-//        System.out.println(cinema.getFilmTitles().size());
+//        CinemaCityRepertoireParser cinema = new CinemaCityRepertoireParser();
+//
         FilmwebFilmParser filmParser = new FilmwebFilmParser("http://www.filmweb.pl/film/%C5%BBo%C5%82nierze+kosmosu-1997-207");
         FilmEntity film = filmParser.getFilm();
         System.out.println(film.getTitle());
         System.out.println(film.getDescription());
-        
+
         HashMap columns = new HashMap();
         columns.put("title", "test");
-        FilmEntity film2 = (FilmEntity) Entity.findOneByEntityAndColumns(new FilmEntity(),columns);
+        FilmEntity film2 = (FilmEntity) Entity.findOneByEntityAndColumns(new FilmEntity(), columns);
         System.out.println(film2.getTitle());
         System.out.println(film2.getDescription());
     }
